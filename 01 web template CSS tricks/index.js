51896fdf2235139b991d2079fbfa8082b2b5f4b2
Domino303d <blockyblock10@gmail.com>
2024-08-23 11:06:28 +0200

    fixed jave sript

--- 01 web template CSS tricks/index.js
+++ 01 web template CSS tricks/index.js
@@ -1,12 +1,33 @@
 let currentPage = 1
+let menuNumber = 1
 let pages //array med alle elementer med class = page
+let menuitems //array med alle menupunkterne
 let colors = ['red', 'purple', 'darkblue', 'gray', 'white', 'green', 'yellow']
 
 
 function setup(){
     console.log("okay buddy lets say that")
+
     select('#page' + currentPage).addClass('visible')
+    select('#menu' + menuNumber).addClass('active')
+    
     pages = selectAll('.page')
+menuitems = selectAll('.menuitem')
+
+for( m of menuitems){
+m.mousePressed( function(e){
+//e.target er selve html div'en
+    console.log(e.target.id)
+    //slice -1 henter det sidste bogstav i en string
+    let nr = e.target.id.slice(-1)
+    shiftPage(nr)
+
+
+} )
+
+}
+
+
     //nu kan man se at pages er blevet til en liste med alle class = page ting
     //console.log(page.length)
 console.log(pages.length)
@@ -14,6 +35,7 @@ console.log(pages.length)
 //lav en masse div'er når vi kommer ind i page 3
 for(c of colors){
     console.log(c)
+
 let div = createDiv()
 div.style("background-color", c)
 select('#page3').child(div)
@@ -37,6 +59,10 @@ function shiftPage(num){
 select("#page" + currentPage).removeClass("visible")
 currentPage = num
 select("#page" + currentPage).addClass("visible")
+
+select("#menu" + menuNumber).removeClass("active")
+menuNumber = num
+select("#menu" + menuNumber).addClass("active")
 }
 
 function keyPressed(){
