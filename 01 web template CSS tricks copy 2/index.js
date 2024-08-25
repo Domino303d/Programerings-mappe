7592641a11aba7b5f2d392e437724b65e4bdc85a
Domino303d <blockyblock10@gmail.com>
2024-08-25 19:28:17 +0200

    cahnges

---
+++ 01 web template CSS tricks copy 2/index.js
@@ -0,0 +1,64 @@
+let currentPage = 1
+
+let pages //array med alle elementer med class = page
+let menuitems //array med alle menupunkterne
+
+function setup(){
+    console.log("okay buddy lets say that")
+    //shiftPage er funktioenn der tag et tal og skifter siden
+
+    pages = selectAll('.page')
+    menuitems = selectAll('.menuitem')
+
+    //menuitems skal reagere med at skifte side
+for( m of menuitems){
+m.mousePressed( function(e){
+//e.target er selve html div'en
+    console.log(e.target.id)
+    //slice -1 henter det sidste bogstav i en string
+    let nr = e.target.id.slice(-1)
+    //nu kan vi kalde shiftPage som skifter side
+    shiftPage(nr)
+
+
+} )
+
+}
+
+
+    //nu kan man se at pages er blevet til en liste med alle class = page ting
+    //console.log(page.length)
+console.log(pages.length)
+
+
+
+shiftPage(currentPage)
+
+
+}
+
+function shiftPage(num){
+    if(num == "ArrowLeft"){
+        num = currentPage - 1
+    }
+    
+    if(num == "ArrowRight"){
+        num = currentPage + 1
+    }
+    
+    if(isNaN(num)|| num > pages.length || num == 0){
+        return
+
+    }
+
+select("#menu" + currentPage).removeClass("active")
+select("#page" + currentPage).removeClass("visible")
+currentPage = num
+select("#page" + currentPage).addClass("visible")
+select("#menu" + currentPage).addClass("active")
+}
+
+function keyPressed(){
+    console.log(key)
+    shiftPage(key)
+}
